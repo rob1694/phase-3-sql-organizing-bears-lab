76be96bc9e95e67e524ee746713c2d1c445fe228
rob1694 <robertb1694@gmail.com>
2021-07-26 04:30:42 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,87 @@
+INSERT INTO bears 
+VALUES (
+    1,
+    'Mr. Chocolate',
+    26,
+    'M',
+    'Brown',
+    'Excited',
+    1
+);
+
+INSERT INTO bears 
+VALUES (
+    2,
+    'Rowdy',
+    21,
+    'F',
+    'black',
+    'Loud',
+    1
+);
+
+INSERT INTO bears 
+VALUES (
+    3,
+    'Tabitha',
+    27,
+    'F',
+    'black',
+    'Quiet',
+    1
+);
+
+INSERT INTO bears 
+VALUES (
+    4,
+    'Sergeant Brown',
+    30,
+    'M',
+    'brown',
+    'Angry',
+    1
+);
+
+INSERT INTO bears 
+VALUES (
+    5,
+    'Melissa',
+    40,
+    'F',
+    'brown',
+    'Pensative',
+    1
+);
+
+INSERT INTO bears 
+VALUES (
+    6,
+    'Grinch',
+    50,
+    'M',
+    'black',
+    'Mischevous',
+    1
+);
+
+INSERT INTO bears 
+VALUES (
+    7,
+    'Wendy',
+    32,
+    'F',
+    'black',
+    'Happy',
+    1
+);
+
+INSERT INTO bears 
+VALUES (
+    8,
+    null,
+    null,
+    null,
+    'black',
+    null,
+    1
+);
